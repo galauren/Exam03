46768c87d3fa2a74e2a86e76593091af638c41a0
Gaylor Laurent <galauren@f2r3s21.clusters.42paris.fr>
2025-07-25 01:24:34 +0200

    some more to go

--- lvl2/rip.c
+++ lvl2/rip.c
@@ -1,39 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rip2.c                                             :+:      :+:    :+:   */
+/*   rip.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: galauren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 00:40:55 by galauren          #+#    #+#             */
-/*   Updated: 2025/07/25 00:47:37 by galauren         ###   ########.fr       */
+/*   Updated: 2025/07/25 01:23:41 by galauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	recursive_identify_parenthesis(char *str, int index)
+int	recursive_remove_opening_parenthesis(char *str, int index)
 {
 	int balance;
 
 	if (!str[index])
 		return (0);
-	balance = recursive_identify_parenthesis(str, index + 1);
+	balance = recursive_remove_opening_parenthesis(str, index + 1);
 	if (str[index] == ')')
-		return (balance + 1);
+		++balance;
 	else if (str[index] == '(')
 	{
 		if (balance == 0)
 			str[index] = ' ';
 		else
-			return (balance - 1);
+			--balance;
 	}
 	return (balance);
 }
 
+int	recursive_remove_closing_parenthesis(char *str, int index, int balance)
+{
+	if (!str[index])
+		return (0);
+	if (str[index] == '(')
+		++balance;
+	else if (str[index] == ')')
+	{
+		if (balance == 0)
+			str[index] = ' ';
+		else
+			--balance;
+	}
+	recursive_remove_closing_parenthesis(str, index + 1, balance);
+	return (0);
+}
+
 void	rip(char *str)
 {
-	recursive_identify_parenthesis(str, 0);
+	recursive_remove_opening_parenthesis(str, 0);
+	recursive_remove_closing_parenthesis(str, 0, 0);
 	puts(str);
 }
 
